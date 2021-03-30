0f3f4d721ef4a4cd77a95afc597c5cd6e22026f0
ruslan547 <novik.rv93@gmail.com>
2021-03-30 21:03:40 +0300

    feat: add lesson35

--- lesson35/Runner35_exm1.java
+++ lesson35/Runner35_exm1.java
@@ -1,5 +1,21 @@
+import by.itClass.sax.UserHandler;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
 public class Runner35_exm1 {
     public static void main(String[] args) {
+        UserHandler handler = new UserHandler();
 
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse("lesson35/itClass.xml", handler);
+        } catch (ParserConfigurationException | SAXException | IOException  e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ lesson35/by/itClass/sax/UserHandler.java
@@ -0,0 +1,34 @@
+package by.itClass.sax;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+//that sax uses UserHandler like tool for pars xml, need to extends from class DefaultHandler
+public class UserHandler extends DefaultHandler {
+    @Override
+    public void startDocument() throws SAXException {
+        System.out.println("START DOCUMENT");
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        System.out.println("END DOCUMENT");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.println("open tag: " + qName);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        System.out.println("close tag:" + qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        String value = new String(ch, start, length);
+        System.out.println("value tag:" + value);
+    }
+}

3cfa5a6aa09012f7a2795b9c5f13806c906f2dac
ruslannovik <novik.rv93@gmail.com>
2021-04-15 21:01:49 +0300

    fix: fix lesson39

--- lesson39_prepare-statement/by/itClass/constants/Constants.java
+++ lesson39_prepare-statement/by/itClass/constants/Constants.java
@@ -1,7 +1,7 @@
 package by.itClass.constants;
 
 public class Constants {
-    public static final String DB_PROPERTIES_FILE = "lesson39/db.properties";
+    public static final String DB_PROPERTIES_FILE = "lesson39_prepare-statement/db.properties";
     public static final String DB_USER = "db.user";
     public static final String  DB_PASSWORD = "db.password";
     public static final String DB_URL = "db.url";
